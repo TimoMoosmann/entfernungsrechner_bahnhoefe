6171ff6910d78d53e94d46ff241d2c91013bfa21
Timo <timo-moosmann@web.de>
2023-01-27 11:21:39 +0100

    Fehlermeldung eindeutiger gemacht.

--- src/main/java/de/web/timo_moosmann/entfernungsrechner_bahnhoefe/bahnhoflocationutils/BahnhofNotFoundException.java
+++ src/main/java/de/web/timo_moosmann/entfernungsrechner_bahnhoefe/bahnhoflocationutils/BahnhofNotFoundException.java
@@ -2,6 +2,6 @@ package de.web.timo_moosmann.entfernungsrechner_bahnhoefe.bahnhoflocationutils;
 
 public class BahnhofNotFoundException extends RuntimeException {
     public BahnhofNotFoundException(String ds100) {
-        super("Could not find Bahnhof with DS100 Code: " + ds100);
+        super("Could not find Fernverkehrsbahnhof with DS100 Code: " + ds100);
     }
 }
